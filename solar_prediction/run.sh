65e9ead52e59707c0569c29e6d4f7229688bc353
DkA18 <karlik.dan@gmail.com>
2025-03-05 16:59:45 +0100

    for ha

---
+++ solar_prediction/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source /opt/venv/bin/activate
+echo "Starting Hasp Add-on..."
+python3 -m flask db init
+python3 -m flask db migrate
+python3 -m flask db upgrade
+redis-server & celery -A make_celery worker --loglevel=info &
+python3 app.py
