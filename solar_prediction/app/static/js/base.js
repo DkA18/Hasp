84fafdb928ee6d21a437b4795466bdd348cef6e6
DkA18 <karlik.dan@gmail.com>
2025-03-07 13:01:33 +0100

    fix

--- solar_prediction/app/static/js/base.js
+++ solar_prediction/app/static/js/base.js
@@ -7,3 +7,15 @@ threeDaysAgo.setDate(today.getDate() - 3);
 const fourDaysLater = new Date();
 fourDaysLater.setDate(today.getDate() + 4);
 
+function changeURL(url){
+    const iframe = document.querySelector("iframe[title='My Iframe Title']");
+if (iframe) {
+    let currentSrc = new URL(iframe.src);
+    currentSrc.searchParams.append('path', url);
+    iframe.src = currentSrc.toString();
+} else {
+    console.log("Iframe with the specified title not found.");
+    window.location.href = url;
+}
+
+}
