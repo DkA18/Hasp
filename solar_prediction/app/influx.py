c0e618aec9357fd36d33f5416928c3e086af40ac
Daniel Karlik <karlik.dan@gmail.com>
2025-03-28 20:04:11 +0100

    test

--- solar_prediction/app/influx.py
+++ solar_prediction/app/influx.py
@@ -18,7 +18,7 @@ def get_influx_data(date_from: datetime.datetime = None, date_to: datetime.datet
             database=ha_options["influx_db"]
         )
         current_app.logger.info(f"Connected to InfluxDB at {ha_options['influx_host']}:{ha_options['influx_port']}")
-        result = client.query(f"""SELECT max("value")  AS "mean_value" FROM "homeassistant"."autogen"."kWh" WHERE {f"time >= '{date_from.strftime('%Y-%m-%dT%H:%M:%SZ')}' AND" if date_from else ""} {f"time < '{(date_to + datetime.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')}' AND" if date_to else ""} "entity_id"='today_s_pv_generation' GROUP BY time(1d) FILL(0)""")
+        result = client.query(f"""SELECT max("value")  AS "mean_value" FROM "homeassistant"."autogen"."kWh" WHERE {f"time >= '{date_from.strftime('%Y-%m-%dT%H:%M:%SZ')}' AND" if date_from else ""} {f"time < '{(date_to + datetime.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')}' AND" if date_to else ""} "entity_id"='today_s_pv_generation' GROUP BY time(1d) FILL(null)""")
         client.close()
     except Exception as e:
         current_app.logger.error(f"Failed to connect to InfluxDB: {e}")
